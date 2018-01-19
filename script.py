9e35c783db9326eee1b2f68d17d7b2c241958a57
Sofiya Semenova <ssemenova@edx.org>
2018-01-19 17:14:18 -0500

    It works

--- script.py
+++ script.py
@@ -1,10 +1,20 @@
-from secrets import tidal_id, tidal_username, tidal_pwd, spotify_id, SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, SPOTIPY_REDIRECT_URI
+from secrets import tidal_id, tidal_username, tidal_pwd, spotify_id, spotify_username, SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, SPOTIPY_REDIRECT_URI
 import requests
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy.util as util
 import sys
 import tidalapi
+
+
+# Options:
+# connect_to_spotify()
+# connect_to_tidal()
+# delete_all_tidal_playlists() -> done
+# move_all_tidal_playlists_to_spotify()
+# move_one_tidal_playlist_to_spotify(playlist_id)
+# move_all_spotify_playlists_to_tidal() -> done
+# move_favourites_from_spotify_to_tidal()
 
 
 # tidalapi does not have these endpoints
@@ -14,17 +24,13 @@ def get_tidal_add_song_to_playlist_url(playlist_id):
     return 'https://listen.tidal.com/v1/playlists/' + playlist_id + '/items'
 def get_tidal_find_song_url():
     return 'https://listen.tidal.com/v1/search/tracks'
-
+def get_tidal_playlist(playlist_id):
+    return 'https://listen.tidal.com/v1/playlists/' + playlist_id
+def get_tidal_user_playlists():
+    return 'https://listen.tidal.com/v1/users/' + tidal_id + '/playlists'
 
 def connect_to_spotify():
     scope = 'user-library-read'
-
-    if len(sys.argv) > 1:
-        spotify_username = sys.argv[1]
-    else:
-        print "Usage: %s username" % (sys.argv[0],)
-        sys.exit()
-
     token = util.prompt_for_user_token(
         spotify_username,
         scope,
@@ -52,8 +58,74 @@ def connect_to_tidal():
     return tidal_session
 
 
-def add_playlist_to_tidal(playlist, tidal_session):
-    playlist_id = _create_playlist(playlist)
+def move_favourites_from_spotify_to_tidal():
+    pass
+
+def delete_all_tidal_playlists():
+    try:
+        r = requests.request(
+            'GET',
+            get_tidal_user_playlists(),
+            headers={
+                'x-tidal-sessionid': tidal_session.session_id,
+                'if-none-match': '*'
+            },
+            params={
+                'countryCode': 'US',
+                'limit': 999
+            }
+        )
+        playlists = [item['uuid'] for item in r.json()['items']]
+    except requests.exceptions.RequestException as e:
+        print("Could not get list of playlists")
+
+    if playlists:
+        for playlist in playlists:
+            try:
+                r = requests.request(
+                    'DELETE',
+                    get_tidal_playlist(playlist),
+                    headers={
+                        'x-tidal-sessionid': tidal_session.session_id,
+                    },
+                    params={
+                        'countryCode': 'US'
+                    }
+                )
+            except requests.exceptions.RequestException as e:
+                print("Could not delete playlist " + playlist_id)
+
+
+def move_all_tidal_playlists_to_spotify():
+    pass
+
+
+def move_one_tidal_playlist_to_spotify(playlist_id):
+    pass
+
+
+def move_all_spotify_playlists_to_tidal():
+    # Get playlists
+    playlists = sp.user_playlists(spotify_id)
+    while playlists:
+        for i, playlist in enumerate(playlists['items']):
+            print("Workin on playlist:" + playlist['name'])
+            if (playlist['owner']['id'] == spotify_id):
+                _add_playlist_to_tidal(playlist, tidal_session)
+        if playlists['next']:
+            playlists = sp.next(playlists)
+        else:
+            playlists = None
+
+    # Get user's saved tracks
+    saved_tracks = sp.current_user_saved_tracks()
+    for item in saved_tracks['items']:
+        track = item['track']
+        favourite_track_in_tidal(item)
+
+
+def _add_playlist_to_tidal(playlist, tidal_session):
+    playlist_id = _create_tidal_playlist(playlist)
 
     if not playlist_id:
         return
@@ -72,81 +144,75 @@ def add_playlist_to_tidal(playlist, tidal_session):
         tracks = sp.next(tracks)
         _add_track_to_sanitized_list(tracks)
 
-    print("DEBUG: list of sanitized tracks: " + str(sanitized_tracks))
     _add_tracks_to_tidal_playlist(playlist_id, sanitized_tracks)
 
 
 def _add_tracks_to_tidal_playlist(playlist_id, tracks):
-    track_ids = []
+    url = get_tidal_add_song_to_playlist_url(playlist_id)
     for name, artist in tracks:
         tidal_track_id = _search_for_track_on_tidal(name, artist)
         if tidal_track_id > -1:
-            track_ids.append(tidal_track_id)
-
-    print("DEBUG: list of track ids: " + str(track_ids))
-    try:
-        r = requests.request(
-            'POST',
-            get_tidal_add_song_to_playlist_url(playlist_id),
-            data={'trackIds':track_ids[0], 'toIndex':1},
-            headers={
-                'x-tidal-sessionid': tidal_session.session_id,
-                'if-none-match': '1'
-            },
-            params={
-                'countryCode': 'US'
-            }
-        )
-
-        # r = requests.request(
-        #     'GET',
-        #     'https://listen.tidal.com/playlist/' + playlist_id,
-        #     headers={
-        #         'x-tidal-sessionid': tidal_session.session_id,
-        #     },
-        #     params={
-        #         'countryCode': 'US'
-        #     }
-        # )
-
-    except requests.exceptions.RequestException as e:
-        print('Error adding tracks to playlist: ' + e)
-        # TODO: should add playlist name to CSV of failures
+            try:
+                r = requests.request(
+                    'POST',
+                    url,
+                    data={'trackIds':tidal_track_id, 'toIndex':1},
+                    headers={
+                        'x-tidal-sessionid': tidal_session.session_id,
+                        'if-none-match': "*"
+                    },
+                    params={
+                        'countryCode': 'US'
+                    }
+                )
+            except requests.exceptions.RequestException as e:
+                print('Error adding tracks to playlist: ' + e)
+                # TODO: should add playlist name to CSV of failures
 
 
 def _search_for_track_on_tidal(name, artist):
     id = -1;
+    artist = artist.lower()
 
     def _artist_in_response(track):
         for response_artist in track['artists']:
-            if response_artist['name'] == artist:
+            if response_artist['name'].lower() == artist:
                 return True
         return False
 
-    try:
-        r = requests.request(
-            'GET',
-            get_tidal_find_song_url(),
-            headers={
-                'x-tidal-sessionid': tidal_session.session_id
-            },
-            params={
-                'offset': 0,
-                'countryCode': 'US',
-                'limit': '50',
-                'query': name
-            }
-        )
+    matched_artist = False
+    offset = 0
+    limit = 300
+    end_of_song_list = False
+    while not matched_artist and not end_of_song_list:
+        try:
+            r = requests.request(
+                'GET',
+                get_tidal_find_song_url(),
+                headers={
+                    'x-tidal-sessionid': tidal_session.session_id
+                },
+                params={
+                    'offset': offset,
+                    'countryCode': 'US',
+                    'limit': limit,
+                    'query': name
+                }
+            )
 
-        matched_artist = False
-        for track in r.json()['items']:
-            if _artist_in_response:
-                id = track['id']
-                matched_artist = True;
-                break;
-    except requests.exceptions.RequestException as e:
-        print('Could not make request for track name=' + name + ", artist=" + artist)
-        # TODO: should add track name to CSV of failures
+            if len(r.json()['items']) == 0:
+                end_of_song_list = True
+
+            for track in r.json()['items']:
+                if _artist_in_response(track):
+                    id = track['id']
+                    matched_artist = True
+                    break
+            offset = offset + 300
+            limit = offset + 300
+        except requests.exceptions.RequestException as e:
+            print('Could not make request for track name=' + name + ", artist=" + artist)
+            # TODO: should add track name to CSV of failures
 
     if not matched_artist:
         print('Could not find track name=' + name + ", artist=" + artist)
@@ -155,7 +221,7 @@ def _search_for_track_on_tidal(name, artist):
     return id;
 
 
-def _create_playlist(playlist):
+def _create_tidal_playlist(playlist):
     title = playlist['name']
 
     try:
@@ -179,33 +245,6 @@ def _create_playlist(playlist):
 
     return r.json()['uuid']
 
-
-def favourite_track_in_tidal(track):
-	pass
-
-
-sp = connect_to_spotify()
-tidal_session = connect_to_tidal()
-
-# Get playlists
-playlists = sp.user_playlists(spotify_id)
-while playlists:
-    for i, playlist in enumerate(playlists['items']):
-        print("DEBUG: on playlist:" + playlist['name'])
-        add_playlist_to_tidal(playlist, tidal_session)
-    if playlists['next']:
-        playlists = sp.next(playlists)
-    else:
-        playlists = None
-
-# Get user's saved tracks
-saved_tracks = sp.current_user_saved_tracks()
-for item in saved_tracks['items']:
-    track = item['track']
-    favourite_track_in_tidal(item)
-
-
-
 # def _add_tracks_to_spotify_playlist():
 #     if len(sys.argv) > 3:
 #         username = sys.argv[1]
@@ -225,3 +264,8 @@ for item in saved_tracks['items']:
 #         print results
 #     else:
 #         print "Can't get token for", username
+
+
+sp = connect_to_spotify()
+tidal_session = connect_to_tidal()
+move_all_spotify_playlists_to_tidal()
